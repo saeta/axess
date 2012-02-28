ee377c5cffe7da4648be0d8e66cb607b86168296
Brennan Saeta <saeta@saeta2.stanford.edu>
2012-02-28 04:30:48 -0800

    Adding useless Trait, adding new messages for new communication.

--- app/com/cautiousfireball/axess/AxessMessage.scala
+++ app/com/cautiousfireball/axess/AxessMessage.scala
@@ -1,5 +1,8 @@
 package com.cautiousfireball.axess
 
-sealed trait AxessMessage
 case class ScanRequest(siteName: String)
 case class ScanResult(siteName: String, scanResult: String)
+case class ScanPage(url: String)
+case class PageNote(url: String, cat: String, msg: String)
+case class PageScanResult(url: String, notes: List[PageNote])
+case class NewUrls(urls: Set[String])
