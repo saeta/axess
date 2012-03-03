d14a9d7a9f7070024ef6ac7c61433b6a53c59019
saeta <saeta@cs.stanford.edu>
2012-03-02 17:00:04 -0800

    Beginning of a new site form.

--- app/controllers/SiteController.scala
+++ app/controllers/SiteController.scala
@@ -9,10 +9,10 @@ import models._
 object SiteController extends Controller {
 
   val newSiteForm = Form(tuple(
+    "tag" -> nonEmptyText,
     "username" -> nonEmptyText,
     "password" -> nonEmptyText,
-    "home" -> nonEmptyText,
-    "dsc" -> text))
+    "home" -> nonEmptyText))
 
   def all = Action {
     Ok(views.html.sites(Site.all()))
@@ -22,4 +22,20 @@ object SiteController extends Controller {
     Redirect(routes.SiteController.all)
   }
 
+  // TODO: create edit and modify forms!
+
+  def newSitePost = Action { implicit request =>
+    newSiteForm.bindFromRequest.fold(
+      errors => {
+        BadRequest(views.html.sitesNew(errors))
+      },
+      good => {
+        Site.create(good._1, good._2, good._3, good._4)
+        Redirect(routes.SiteController.all)
+      })
+  }
+
+  def newSiteGet = Action { implicit request =>
+    Ok(views.html.sitesNew(newSiteForm))
+  }
 }
