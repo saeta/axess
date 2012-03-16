ffd29aeb43f2add971219b5cd146483221288381
saeta <saeta@cs.stanford.edu>
2012-03-15 23:14:49 -0700

    Removing failed class loading folly.

--- project/Build.scala
+++ project/Build.scala
@@ -11,8 +11,7 @@ object ApplicationBuild extends Build {
       // Add your project dependencies here,
       "org.seleniumhq.selenium" % "selenium-server" % "2.19.0",
       "mysql" % "mysql-connector-java" % "5.1.18",
-      "org.apache.httpcomponents" % "httpclient" % "4.1.3",
-      "org.clapper" %% "classutil" % "0.4.4"
+      "org.apache.httpcomponents" % "httpclient" % "4.1.3"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
