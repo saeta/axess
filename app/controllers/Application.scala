cf4ba6d7e54f897c0e6609a5b06464341fe35371
saeta <saeta@cs.stanford.edu>
2012-03-20 11:53:00 -0700

    Clean up old tinkercruft.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -15,31 +15,7 @@ import models.DashboardEntry
 
 object Application extends Controller {
 
-  class SimpleResponder extends Actor {
-    def receive = {
-      case "ping" => sender ! "pong"
-    }
-  }
-
-  val system = ActorSystem("Axess")
-  val sr = system.actorOf(Props[SimpleResponder], "simpleResponder")
-  val axess = system.actorOf(Props[Axess], "axess")
-  implicit val timeout = Timeout(1000 milliseconds)
-
   def index = Action {
     Ok(views.html.index(DashboardEntry.get()))
   }
-
-  def foo = Action {
-    AsyncResult {
-      (sr ? "ping").mapTo[String].asPromise.map { r => Ok("Response: " + r) }
-    }
-  }
-
-  def bar = Action {
-    AsyncResult {
-      (axess ? "Hello").mapTo[String].asPromise.map { r => Ok("Response: " + r) }
-    }
-  }
-
 }
