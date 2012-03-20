a07d145dd77c4fc1f0a4a8752df31ac55fc37a86
saeta <saeta@cs.stanford.edu>
2012-03-20 14:57:21 -0700

    Add System Status to the top nav bar.

--- app/views/NavbarHeaders.scala
+++ app/views/NavbarHeaders.scala
@@ -4,6 +4,7 @@ object NavbarHeaders {
   val defaults: List[(play.api.mvc.Call, String)] = List(
     (controllers.routes.SiteController.all(), "Sites"),
     (controllers.routes.ScanController.summary(), "Scan Results"),
-    (controllers.routes.Application.successes(), "Successes")
+    (controllers.routes.Application.successes(), "Successes"),
+    (controllers.routes.ScanController.sysstatus(), "Status")
   )
 }
