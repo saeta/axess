451550a40895c5fa85b99fcc425c9b532049ef74
saeta <saeta@cs.stanford.edu>
2012-03-20 11:55:28 -0700

    Clean up import tinkercruft.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,16 +1,6 @@
 package controllers
 
-import play.api._
 import play.api.mvc._
-import play.api.libs.concurrent._
-import akka.actor.ActorSystem
-import akka.actor.Actor
-import akka.actor.Props
-import akka.pattern._
-import akka.util.duration._
-import akka.util.Timeout
-import axess._
-import play.api.Play._
 import models.DashboardEntry
 
 object Application extends Controller {
