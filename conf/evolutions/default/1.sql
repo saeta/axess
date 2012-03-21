1308f3f70abb54a71de3c04b0436a0f552f2bec3
saeta <saeta@cs.stanford.edu>
2012-03-21 09:28:24 -0700

    Make the sql be h2-friendly.

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -3,13 +3,13 @@
 # --- !Ups
 
 CREATE TABLE Sites (
-  id MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY, -- pk
-  tag VARCHAR(50) NOT NULL, -- short human-readable tag
-  username VARCHAR(100) NOT NULL, -- login info
-  password VARCHAR(100) NOT NULL,
-  home VARCHAR(100) NOT NULL, -- home page url
-  type VARCHAR(1000) NOT NULL, -- Class name for site
-  dsc VARCHAR(1000) NOT NULL DEFAULT "" -- description
+  `id` MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY, -- pk
+  `tag` VARCHAR(50) NOT NULL, -- short human-readable tag
+  `username` VARCHAR(100) NOT NULL, -- login info
+  `password` VARCHAR(100) NOT NULL,
+  `home` VARCHAR(100) NOT NULL, -- home page url
+  `type` VARCHAR(1000) NOT NULL, -- Class name for site
+  `dsc` VARCHAR(1000) NOT NULL DEFAULT ''-- description
 );
 
 # --- !Downs
